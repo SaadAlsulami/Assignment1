9e1f3cf009a23f656ca5b591457602cfd5698924
SaadAlsulami <SaadBAlsulami@gmail.com>
2024-10-19 19:30:53 +0300

    Add files via upload

---
+++ Assignment 1/Assignment1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class Calculator
+{
+
+ public static void Main(string[] args)
+ {
+    Console.WriteLine("Enter a number1: ");
+
+    int num1 = Convert.ToInt32(Console.ReadLine());
+    
+    Console.WriteLine("Enter a number2: ");
+
+    int num2 = Convert.ToInt32(Console.ReadLine());
+    int SumNum = num1 + num2;
+    Console.WriteLine(SumNum);
+    // Saad
+
+
+ }
+ 
+
+
+}
